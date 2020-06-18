6be04c16b14985cfa0eaf0d6ba298e649ac80929
Rane <github@blasph.me>
2020-06-18 15:17:40 -0700

    Initialize and install dependencies

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "client")));
+
+app.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
+});
