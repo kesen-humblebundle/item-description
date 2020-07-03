cbe5cb047c577cfa0916160bed057afa641ee88c
Rane Wallin <github@blasph.me>
2020-07-03 16:12:57 -0700

    Adding toggle functionality and more styles

--- client/src/description.js
+++ client/src/description.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import styled from "styled-components";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 
 import { getDescription } from "../utils/get_description";
 import { getImages } from "../utils/get_images";
 
 const Description = ({ productID }) => {
   let [description, setDescription] = useState("");
+  let [title, setTitle] = useState("");
+  let [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
     let desc = getDescription(productID).then((resp) => {
@@ -16,7 +18,7 @@ const Description = ({ productID }) => {
           <ParagraphWrapper key={"p" + i}>{para}</ParagraphWrapper>
         ));
 
-      setDescription(paragraphs);
+      setTitle(resp.title);
       getImages().then((imageUrls) => {
         let images = imageUrls.map((url, i) => (
           <ImageWrapper key={"img" + i}>
@@ -35,7 +37,23 @@ const Description = ({ productID }) => {
       });
     });
   }, []);
-  return <DescWrapper>{description}</DescWrapper>;
+  return (
+    <div>
+      <DescWrapper expanded={expanded}>{description}</DescWrapper>
+      <DescToggle onClick={() => setExpanded(!expanded)}>
+        <span>
+          {expanded ? "Show less" : "Read more"} about {title}{" "}
+        </span>
+        <IconWrapper>
+          {expanded ? (
+            <IoMdArrowDropup size="1.8em" />
+          ) : (
+            <IoMdArrowDropdown size="1.8em" />
+          )}
+        </IconWrapper>
+      </DescToggle>
+    </div>
+  );
 };
 
 export default Description;
@@ -69,4 +87,20 @@ const DescWrapper = styled.div`
   justify-content: space-around;
   align-items: center;
   align-content: center;
+  height: ${(props) => (props.expanded === false ? "345px" : "auto")};
+  overflow: hidden;
+`;
+
+const DescToggle = styled.div`
+  font-family: "Sofia Pro", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  margin: 20px;
+  font-weight: 900;
+  background-color: #e4e7ed;
+  color: #494f5c;
+`;
+
+const IconWrapper = styled.div`
+  display: inline-block;
+  height: 100%;
 `;
