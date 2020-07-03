2c359136831378b19cae7a4ed46286c0ff9559a7
Rane Wallin <github@blasph.me>
2020-07-03 14:11:58 -0700

    Stub in desc component

--- client/app.js
+++ client/app.js
@@ -3,8 +3,10 @@ import React from "react";
 import axios from "axios";
 import styled from "styled-components";
 
+import Description from "./src/description";
+
 const App = () => {
-  return <div></div>;
+  return <Description />;
 };
 
 export default App;
