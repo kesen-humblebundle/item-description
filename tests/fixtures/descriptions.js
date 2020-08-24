bcca9e2af4470e8740a15a3743e6db942783db28
Christina Wang <cywang117@outlook.com>
2020-08-24 16:56:12 +0900

    Refactor to integration tests, setup test db & env config

---
+++ tests/fixtures/descriptions.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    product_id: 1,
+    title: "Cool game The Original",
+    description: "A cool game",
+    genre: ["Adventure"]
+  },
+  {
+    product_id: 2,
+    title: "Cool game II",
+    description: "More cool game",
+    genre: ["FPS"]
+  },
+  {
+    product_id: 3,
+    title: "Cool game 3D",
+    description: "The coolest game",
+    genre: ["MMO", "Multiplayer", "Puzzle"]
+  },
+  {
+    product_id: 4,
+    title: "Cool game 4",
+    description: "2 cool, 2 game",
+    genre: ["Sports"]
+  },
+  {
+    product_id: 5,
+    title: "Cool game's revenge",
+    description: "Look out cool game",
+    genre: ["Action", "RPG"]
+  },
+];
