719bb8a5d38ef3ab582b3f91626c1f9a2342af92
Christina Wang <cywang117@outlook.com>
2020-08-24 16:51:31 +0900

    Update/remove EC2 endpoints
    Image API endpoint remains nonfunctional since it requires another service's
    EC2 instance which appears to be down, so UI component will not have
    an image display.

--- client/utils/get_description.js
+++ client/utils/get_description.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 
-const API_URL =
-  location.origin === "http://ec2-54-224-38-115.compute-1.amazonaws.com:5150"
-    ? ""
-    : "http://ec2-54-224-38-115.compute-1.amazonaws.com:5150";
-
 export const getDescription = async (product_id) => {
-  let desc = await axios.get(`${API_URL}/description/${product_id}`);
+  let desc = await axios.get(`/description/${product_id}`);
   return desc.data;
 };
