b5a5ab9e69504c0f10c978294eb9a1bc2f5cc4f6
Christina Wang <cywang117@outlook.com>
2020-09-04 19:25:49 +0900

    Modify tests to accept MariaDB as DB

--- tests/globalTestTeardown.js
+++ tests/globalTestTeardown.js
@@ -1,30 +1,30 @@
-const knex = require('knex');
+require('dotenv').config();
+const mariaDB = require('mariadb');
 
 // Almost exact same setup as globalTestSetup.js. Note that user: postgres is the default user
 // for Windows WSL and Linux. It's uncertain if this knex config will work for Mac, since the default superuser
 // for Mac's Postgres service isn't 'postgres', but rather the user's username.
 const dropDB = async () => {
-  const dbConnection = knex({
-    client: 'postgres',
-    debug: true,
-    connection: {
-      host: '127.0.0.1',
-      database: 'postgres',
-      port: '5432',
-      password: '',
-      user: 'postgres'
-    }
+  let conn = await mariaDB.createConnection({
+    host: process.env.MDBHOST,
+    user: process.env.MDBUSER,
+    password: process.env.MDBPASS,
+    port: process.env.MDBPORT,
+    permitLocalInfile: true
   });
 
   try {
-    await dbConnection.raw('DROP DATABASE IF EXISTS desc_service_test');
+    await conn.query('DROP DATABASE IF EXISTS item_descriptions_test');
   } catch (e) {
     console.error(e);
   } finally {
-    await dbConnection.destroy();
+    await conn.destroy();
   }
+  return true;
 }
 
 module.exports = async () => {
   await dropDB();
+  // Prevent mysterious hanging after 'Ran all test suites' message in CLI
+  process.exit(0);
 }
